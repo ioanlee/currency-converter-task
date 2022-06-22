7799116926fcfde5859c50c6c3a861c8fdeab819
ioanlee <reddaravus@gmail.com>
2022-06-22 14:38:36 +1000

    recreate app with vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import TodoList from './TodoList'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <TodoList />
+    <input type="text" />
+    <button>Add todo</button>
+    <button>Clear Completed todos</button>
+    <div>0 left to do</div>
+    </>
+  )
 }
 
-export default App;
+export default App
