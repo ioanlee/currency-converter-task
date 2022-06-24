37054a646c00d1b04f392984edae95cd0d50b6c3
ioanlee <reddaravus@gmail.com>
2022-06-24 16:36:45 +1000

    general fixes

--- src/CurrConverter.tsx
+++ src/CurrConverter.tsx
@@ -9,29 +9,89 @@ interface Props {
 
 export const CurrConverter = ({ currencyOptions }: Props) => {
 
+  // const [fromAmount, setFromAmount] = useState(1)
+  // const [toAmount, setToAmount] = useState(1)
+  const [amount, setAmount] = useState(1)
   const [fromCurrency, setFromCurrency] = useState('')
   const [toCurrency, setToCurrency] = useState('')
-
+  const [exchangeRate, setExchangeRate] = useState(1)
+  const [isFromAmount, setIsFromAmount] = useState(true)
   
   useEffect(() => {
     setFromCurrency(Object.keys(currencyOptions)[0])
-    setToCurrency(Object.keys(currencyOptions)[0])
+    setToCurrency(Object.keys(currencyOptions)[1])
+    setExchangeRate(currencyOptions[toCurrency])
   }, [])
+
+  let fromAmount:number, toAmount:number
+  if (isFromAmount) {
+    fromAmount = Number(amount)
+    toAmount = Number((amount * exchangeRate).toFixed(2))
+  } else {
+    toAmount = Number(amount)
+    fromAmount = Number((amount / exchangeRate).toFixed(2))
+  }
+
+  useEffect(() => {
+    if (fromCurrency && toCurrency) {
+      setIsFromAmount(false)
+      setExchangeRate(currencyOptions[fromCurrency])
+    }
+  }, [fromCurrency])
+  
+  useEffect(() => {
+    if (fromCurrency && toCurrency) {
+      setIsFromAmount(true)
+      setExchangeRate(currencyOptions[toCurrency])
+    }
+  }, [toCurrency])
+
+  function reverse() {
+
+    // setIsFromAmount(!isFromAmount)
+    // const saved = fromCurrency
+    // setFromCurrency(toCurrency)
+    // setToCurrency(saved)
+
+
+    // console.log({ fromCurrency, toCurrency, amount, fromAmount, toAmount, exchangeRate })
+    // (isFromAmount) ? {setAmount(toAmount)} : {setAmount(fromAmount)}
+    // setExchangeRate(1 / currencyOptions[fromCurrency] * currencyOptions[toCurrency])
+  }
+  const handleChangeToAmount = (e) => {
+    setAmount(e.target.value)
+    setIsFromAmount(false)
+  }
+  const handleChangeFromAmount = (e) => {
+    setAmount(e.target.value)
+    setIsFromAmount(true)
+  }
+  const handleChangeToCurrency = (e) => {
+    setToCurrency(e.target.value)
+  }
+  const handleChangeFromCurrency = (e) => {
+    setFromCurrency(e.target.value)
+  }
 
   return (
     <div>
       <h2>Converter</h2>
       <CurrConverterRow 
-        currencyOptions={currencyOptions}
+        amount={fromAmount}
         selected={fromCurrency}
-        onChangeCurrency={e => setFromCurrency(e.target.value)}
-      />
-      =
-      <CurrConverterRow 
         currencyOptions={currencyOptions}
-        selected={toCurrency}
-        onChangeCurrency={e => setToCurrency(e.target.value)}
+        onChangeAmount={handleChangeFromAmount}
+        onChangeCurrency={handleChangeFromCurrency}
       />
+      {`1 ${fromCurrency} = ${Number((1 / currencyOptions[fromCurrency] * currencyOptions[toCurrency]).toFixed(2))} ${toCurrency}`}
+      <CurrConverterRow 
+        amount={toAmount}
+        selected={toCurrency}
+        currencyOptions={currencyOptions}
+        onChangeAmount={handleChangeToAmount}
+        onChangeCurrency={handleChangeToCurrency}
+      />
+      <button onClick={reverse}>Reverse currency</button>
     </div>
   )
 }
